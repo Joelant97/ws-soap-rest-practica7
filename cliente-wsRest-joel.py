898ebc45878fdc03328ed9a8d717f98c16e331f1
Joelant97 <joelant97@gmail.com>
2018-07-27 17:41:09 -0400

    update

--- cliente-wsRest-joel.py
+++ cliente-wsRest-joel.py
@@ -4,16 +4,16 @@ import json
 
 #Listar Todos los Estudiantes: 
 
-listEstudiantes= unirest.get("http://localhost:4567/wsRest/estudiantes/", headers={ "Accept": "application/json" })
+listEstudiantes= unirest.get("http://localhost:4567/rest/estudiantes/", headers={ "Accept": "application/json" })
 
 print "Estudiantes: \n" + str(listEstudiantes.body)
 
 #Consultar Estudiante: 
 
-estudiante= unirest.get("http://localhost:4567/wsRest/estudiantes/" + str(20121776), headers={ "Accept": "application/json" })
+estudiante= unirest.get("http://localhost:4567/rest/estudiantes/" + str(20121776), headers={ "Accept": "application/json" })
 print "Consulta correcta: " + str(estudiante.body)
 
 #Crear un nuevo Estudiante: 
-crearEstudiantes= unirest.post("http://localhost:4567/wsRest/estudiantes/", headers={ "Accept": "application/json"}, params=json.dumps({ "nombre": "Joel", "carrera": "isc", "correo": "joelant@gmail.com"}))
+crearEstudiantes= unirest.post("http://localhost:4567/rest/estudiantes/", headers={ "Accept": "application/json"}, params=json.dumps({ "nombre": "Joel", "carrera": "isc", "correo": "joelant@gmail.com"}))
 
 print "Se creo el nuevo estudiante: " + str(crearEstudiantes.body)
